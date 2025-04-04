02926a43aa2e615547fcd19379b03f72afeee2c6
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-04-04 14:03:08 -0300

    criacao do formulario de novos clientes

---
+++ front/src/app/clients/client.models.ts
@@ -0,0 +1,13 @@
+export type ClientModelForm = {
+    id?: number
+    name: string
+    email: string
+    phone: string
+}
+
+export type ClientModelTable = {
+    id: number
+    name: string
+    email: string
+    phone: string
+}
--- front/src/app/clients/components/client-form/client-form.component.ts
+++ front/src/app/clients/components/client-form/client-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ClientModelForm } from '../../client.models';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-client-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    NgxMaskDirective
+  ],
   templateUrl: './client-form.component.html',
   styleUrl: './client-form.component.css'
 })
 export class ClientFormComponent {
+
+  @Input() client: ClientModelForm = { id: 0, email: '', name: '', phone: ''};
+  @Output() clientSubmited = new EventEmitter<ClientModelForm>();
+
+  onSubmit(_: NgForm) {
+    this.clientSubmited.emit(this.client);
+  }
 
 }
