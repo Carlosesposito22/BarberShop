c54353e2e82c9daf8cefff5d3f8c4f4724cf6648
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-04-05 15:35:39 -0300

    Start project API

---
+++ back/barber-shop-ui/src/main/java/br/com/dio/barbershopui/entity/ScheduleEntity.java
@@ -0,0 +1,55 @@
+package br.com.dio.barbershopui.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(
+        name = "SCHEDULES",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_SCHEDULE_INTERVAL", columnNames = {"start_at", "end_at"}),
+        }
+)
+@Getter
+@Setter
+@ToString
+public class ScheduleEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "start_at")
+    private OffsetDateTime startAt;
+
+    @Column(nullable = false, name = "ent_at")
+    private OffsetDateTime endAt;
+
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private ClientEntity clientEntity = new ClientEntity();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ScheduleEntity that = (ScheduleEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(startAt, that.startAt) && Objects.equals(endAt, that.endAt);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(startAt);
+        result = 31 * result + Objects.hashCode(endAt);
+        return result;
+    }
+}
