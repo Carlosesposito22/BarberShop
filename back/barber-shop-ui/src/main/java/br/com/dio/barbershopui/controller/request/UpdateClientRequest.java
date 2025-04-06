3e7548704a24f72cad3019f9a9e198644884a860
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-04-05 22:03:34 -0300

    mapeamento dos objetos do controller

---
+++ back/barber-shop-ui/src/main/java/br/com/dio/barbershopui/controller/request/UpdateClientRequest.java
@@ -0,0 +1,16 @@
+package br.com.dio.barbershopui.controller.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateClientRequest(
+        @NotNull
+        @JsonProperty("names")
+        String name,
+        @NotNull
+        @JsonProperty("email")
+        String email,
+        @NotNull
+        @JsonProperty("phone")
+        String phone
+) {}
